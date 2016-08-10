1dead8ebaad5020bec7792b76cc0dcbb1e88a830
根東秀都 <shuto8201@gmail.com>
2016-08-10 15:25:17 +0900

    define routing

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'ProductsController@index');
+
+Route::get('/products/search/', 'ProductsController@search');
+
+Route::resource('products', 'ProductsController', ['only' => 'show']);
