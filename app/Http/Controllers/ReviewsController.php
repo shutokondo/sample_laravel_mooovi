db69236d702a1edd4df96afefc30b0a4cbad1e42
根東秀都 <shuto8201@gmail.com>
2016-08-10 15:24:40 +0900

    define action method to controllers

--- app/Http/Controllers/ReviewsController.php
+++ app/Http/Controllers/ReviewsController.php
@@ -6,7 +6,17 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
-class ReviewsController extends Controller
+class ReviewsController extends RankingController
 {
-    //
+    public function create()
+    {
+        $product = Product::find($id);
+        $review = new Review();
+    }
+
+    public function store()
+    {
+        // Review::create()
+        // トップページにリダイレクトする
+    }
 }
